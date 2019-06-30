20b5e185676aa0da5538b8a936712aabe3e5d7e4
Yassine <ouhdid@gmail.com>
2019-06-30 15:54:18 +0100

    *.php

---
+++ include/header.php
@@ -0,0 +1,114 @@
+        
+        <!-- MOBILE MENU-->
+        <!-- ==========================-->
+        <div data-off-canvas="mobile-slidebar left overlay">
+            <a class="navbar-brand scroll" href="home.html">
+                <img class="normal-logo img-resonsive visible-xs visible-sm" src="assets/logo-dark.svg" alt="logo" />
+                <img class="scroll-logo img-resonsive hidden-xs hidden-sm" src="assets/logo-dark.svg" alt="logo" />
+            </a>
+            
+        </div>
+        <div class="l-theme animated-css" data-header="sticky" data-header-top="200" data-canvas="container">
+    
+            <!-- ==========================-->
+            <!-- SEARCH MODAL-->
+            <!-- ==========================-->
+            <div class="header-search open-search">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
+                            <div class="navbar-search">
+                                <form class="search-global">
+                                    <input class="search-global__input" type="text" placeholder="Type to search" autocomplete="off" name="s" value="" />
+                                    <button class="search-global__btn"><i class="icon stroke icon-Search"></i>
+                                    </button>
+                                    <div class="search-global__note">Begin typing your search above and press return to search.</div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="search-close close" type="button"><i class="fa fa-times"></i>
+                </button>
+            </div>
+            <div data-off-canvas="slidebar-1 left overlay">
+                <ul class="nav navbar-nav">
+                    <li><a class="scrollTo" href="">features</a>
+                    </li>
+                    <li><a class="scrollTo" href="">Services</a>
+                    </li>
+                    <li><a class="scrollTo" href="">Works</a>
+                    </li>
+                    <li><a class="scrollTo" href="">About</a>
+                    </li>
+                    <li><a class="scrollTo" href="">Team</a>
+                    </li>
+                
+                   
+                </ul>
+            </div>
+            <header class="header header-boxed-width navbar-fixed-top header-background-white header-color-black header-topbar-dark header-logo-black header-topbarbox-1-left header-topbarbox-2-right header-navibox-1-left header-navibox-2-right header-navibox-3-right header-navibox-4-right">
+                <div class="container container-boxed-width">
+                    <div class="top-bar">
+                        <div class="container">
+                            <div class="header-topbarbox-1">
+                                <ul>
+                                    <li><i class="icon fa fa-clock-o"></i> Mon - Fri : 0900am to 0600pm</li>
+                                    <li><i class="icon fa fa-phone"></i><a href="tel:+0427983549">+ 042 798 3549</a>
+                                    </li>
+                                    <li><i class="icon fa fa-envelope-o"></i><a href="mailto:support@carsrentalantalya.com">support@carsrentalantalya.com</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="header-topbarbox-2">
+                                <ul class="social-links">
+                                    <li><a href="/" target="_blank"><i class="social_icons fa fa-twitter"></i></a>
+                                    </li>
+                                    <li><a href="/" target="_blank"><i class="social_icons fa fa-facebook"></i></a>
+                                    </li>
+                                    <li><a href="/" target="_blank"><i class="social_icons fa fa-linkedin"></i></a>
+                                    </li>
+                                    <li class="li-last"><a href="/" target="_blank"><i class="social_icons fa fa-instagram"></i></a>
+                                    </li>
+                                    <li><a href="/" target="_blank"><i class="social_icons fa fa-youtube-play"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <nav class="navbar" id="nav">
+                        <div class="container">
+                            <div class="header-navibox-1">
+                                <!-- Mobile Trigger Start-->
+                                <button class="menu-mobile-button visible-xs-block js-toggle-mobile-slidebar toggle-menu-button"><i class="toggle-menu-button-icon"><span></span><span></span><span></span><span></span><span></span><span></span></i>
+                                </button>
+                                <!-- Mobile Trigger End-->
+                                <a class="navbar-brand scroll" href="">
+                                    <img class="normal-logo img-responsive" src="" alt="" />
+                                    <img class="scroll-logo hidden-xs img-responsive" src="" alt="" />
+                                </a>
+                            </div>
+                            <div class="header-navibox-3">
+                                <ul class="nav navbar-nav hidden-xs clearfix vcenter">
+                                   
+                                  
+                            </div>
+                            <div class="header-navibox-2">
+                                <ul class="yamm main-menu nav navbar-nav">
+                                    <li><a  href="index.html">Home</a></li>
+                                    <li><a  href="listings.html">Car Listing</a></li>
+                                    <li><a  href="#">About</a></li>
+                                    <li><a  href="blog-main.html">News</a></li>
+                                    <li><a  href="#">Contact</a></li>
+                                    <li><a  href="#">FAQ</a></li>
+                                    <li><a  href="#">HOW IT WORKS</a></li>
+                                 
+                                    
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </header>
+            <!-- end .header-->
+          
